9a4b678b678b6e9921d54d735d9fee03353a2959
An <an@clab>
2020-09-12 13:44:42 +0300

    Longest common sequence draft

--- 1/longest_common_subsequence.py
+++ 1/longest_common_subsequence.py
@@ -9,18 +9,37 @@ def main():
     m = len(X)
     n = len(Y)
     c_matrix = np.zeros(shape=(m + 1, n + 1))
-    b_matrix = np.zeros(shape=(m, n), dtype= np)
+    b_matrix = np.zeros(shape=(m, n), dtype= np.byte)
     for i in range(1, m + 1):
         for j in range(1, n + 1):
             if X[i - 1] == Y[j - 1]:
                 c_matrix[i][j] = c_matrix[i - 1][j - 1] + 1
-                # todo
+                b_matrix[i - 1][j - 1] = 1
             elif c_matrix[i - 1][j] >= c_matrix[i][j - 1]:
                 c_matrix[i][j] = c_matrix[i - 1][j]
+                b_matrix[i - 1][j - 1] = 0
             else:
                 c_matrix[i][j] = c_matrix[i][j - 1]
-                # todo
+                b_matrix[i - 1][j - 1] = -1
     print(c_matrix)
+    print(b_matrix)
+
+    longest_com_subseq = []
+    i = m - 1
+    j = n - 1
+    while i != 0 or j != 0:
+    # if i == 0 or j == 0:
+        # return
+        print(i, j)
+        if b_matrix[i][j] == 1:
+            longest_com_subseq.append(c_matrix[i + 1][j + 1])
+            i -= 1
+            j -= 1
+        elif b_matrix[i][j] == 0:
+            i -= 1
+        else:
+            j -= 1
+    print(longest_com_subseq)
 
 
 if __name__ == '__main__':
