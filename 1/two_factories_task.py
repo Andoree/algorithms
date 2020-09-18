3a38da62dec85cead465e5448731b17df35fed25
An <an@clab>
2020-09-18 12:08:14 +0300

    Two factories task

---
+++ 1/two_factories_task.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+
+def main():
+    # a_0 = [3, 5, 3, 10, 5, 11, 1, ]
+    a_0 = [int(x) for x in input
+        ("Введите стоимости путей нулевой страны:\n")]
+    n = len(a_0)
+    # a_1 = [6, 2, 1, 8, 5, 9, 10, ]
+    a_1 = [int(x) for x in input
+        ("Введите стоимости путей первой страны:\n")]
+    # f_0_1 = [1, 5, 7, 3, 10, 11]
+    f_0_1 = [int(x) for x in input
+        ("Введите стоимости переходов из нулевой страны в первую:\n")]
+    # f_1_0 = [4, 2, 7, 8, 9, 5]
+    f_1_0 = [int(x) for x in input
+        ("Введите стоимости переходов из первой страны в нулевую:\n")]
+    b_0 = np.zeros(shape=n, dtype=np.bool)
+    b_1 = np.zeros(shape=n, dtype=np.bool)
+
+    c_0_list = []
+    c_1_list = []
+    forward_pass_costs = [a_0, a_1]
+    change_path_costs = [f_0_1, f_1_0]
+    change_path_flags = [b_0, b_1]
+    aggregated_costs = [c_0_list, c_1_list]
+    for i in range(0, n):
+        for j in range(2):
+            if i == 0:
+                aggregated_costs[j].append(forward_pass_costs[j][i])
+            else:
+                forward_cost = aggregated_costs[j][i - 1] + forward_pass_costs[j][i]
+                round_cost = aggregated_costs[(j + 1) % 2][i - 1] + \
+                             change_path_costs[(j + 1) % 2][i - 1] + forward_pass_costs[j][i]
+                min_cost = min(forward_cost, round_cost)
+                aggregated_costs[j].append(min_cost)
+                change_path_flags[j][i] = True if round_cost < forward_cost else 0
+
+    optimal_path = []
+    initial_node = 0 if aggregated_costs[0][-1] < aggregated_costs[1][-1] else 1
+    optimal_path.append((initial_node, -1))
+    for i in range(n - 1, -1, -1):
+        optimal_path.append((initial_node, initial_node))
+        if change_path_flags[initial_node][i] == True:
+            previous_node = (initial_node + 1) % 2
+            optimal_path.append((previous_node, initial_node))
+            initial_node = previous_node
+    optimal_path.append((-1, initial_node))
+    optimal_path.reverse()
+    for (from_node, to_node) in optimal_path:
+        if to_node == -1:
+            print(f"End: {from_node}")
+        elif from_node == -1:
+            print(f"Start: {to_node}")
+        elif from_node != to_node:
+            print(f"Change path: {from_node} -> {to_node}")
+        else:
+            print(f"Go forward: {from_node} -> {to_node}")
+
+
+if __name__ == '__main__':
+    main()
