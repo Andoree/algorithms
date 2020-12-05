b90477ffd0f998988c554fed44bda067167b7215
Andoree <and2900@inbox.ru>
2020-12-06 01:19:21 +0300

    Kazan tickets task

---
+++ 5/kazan_tickets.py
@@ -0,0 +1,70 @@
+import time
+
+import numpy as np
+
+factorials_memory = [1]
+
+def factorial(n):
+    if len(factorials_memory) <= n:
+        for i in range(len(factorials_memory), n + 1):
+            factorials_memory.append(factorials_memory[i - 1] * i)
+
+    return factorials_memory[n]
+
+
+def is_integers_list_splitable(numbers_list):
+    list_length = len(numbers_list)
+    list_sum = sum(numbers_list)
+
+    if list_sum % 2 == 1:
+        return False
+    solution_matrix = np.empty(shape=(list_sum // 2 + 1, list_length + 1), dtype=np.bool)
+    for i in range(solution_matrix.shape[1]):
+        solution_matrix[0][i] = True
+    for i in range(1, solution_matrix.shape[0]):
+        solution_matrix[i][0] = False
+    for i in range(1, solution_matrix.shape[0]):
+        for j in range(1, solution_matrix.shape[1]):
+            if i - numbers_list[j - 1] >= 0:
+                solution_matrix[i][j] = solution_matrix[i][j - 1] or solution_matrix[i - numbers_list[j - 1]][j - 1]
+            else:
+                solution_matrix[i][j] = solution_matrix[i][j - 1]
+    return solution_matrix[solution_matrix.shape[0] - 1][solution_matrix.shape[1] - 1]
+
+
+def main():
+    ticket_length = 6
+    num_tickets = 0
+    start_time = time.time()
+
+    for num_0s in range(ticket_length + 1):
+        for num_1s in range(ticket_length - num_0s + 1):
+            for num_2s in range(ticket_length - (num_0s + num_1s) + 1):
+                for num_3s in range(ticket_length - (num_0s + num_1s + num_2s) + 1):
+                    for num_4s in range(ticket_length - (num_0s + num_1s + num_2s + num_3s) + 1):
+                        for num_5s in range(ticket_length - (num_0s + num_1s + num_2s + num_3s + num_4s) + 1):
+                            for num_6s in range(
+                                    ticket_length - (num_0s + num_1s + num_2s + num_3s + num_4s + num_5s) + 1):
+                                for num_7s in range(ticket_length - (
+                                        num_0s + num_1s + num_2s + num_3s + num_4s + num_5s + num_6s) + 1):
+                                    for num_8s in range(ticket_length - (
+                                            num_0s + num_1s + num_2s + num_3s + num_4s + num_5s + num_6s + num_7s) + 1):
+                                        ticket_digit_counts = [num_0s, num_1s, num_2s, num_3s, num_4s, num_5s,
+                                                               num_6s, num_7s, num_8s, ]
+                                        num_9s = max(ticket_length - sum(ticket_digit_counts), 0)
+                                        ticket_digit_counts.append(num_9s)
+                                        assert sum(ticket_digit_counts) == ticket_length
+                                        ticket_digits = []
+                                        for i in range(len(ticket_digit_counts)):
+                                            ticket_digits.extend([i] * ticket_digit_counts[i])
+                                        if is_integers_list_splitable(ticket_digits):
+                                            num_permutations = factorial(ticket_length)
+                                            for multiplicity in ticket_digit_counts:
+                                                num_permutations = num_permutations // factorial(multiplicity)
+                                            num_tickets += num_permutations
+    print(num_tickets)
+    print(f"{time.time() - start_time:.2f}")
+
+
+if __name__ == '__main__':
+    main()
